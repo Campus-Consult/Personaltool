bdf5b78f53296a14c672070c65fb70d2e5b81fef
Andreas Pingel <a_pingel@mail.uni-paderborn.de>
2020-06-08 20:46:23 +0200

    Added necessary Attributes in Person.cs

--- Models/Person.cs
+++ Models/Person.cs
@@ -12,10 +12,31 @@ namespace Personaltool.Models
     public class Person
     {
         [Key]
-        public int PersonID { get; set; }
+        public string PersonID { get; set; }
 
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public DateTime Birthdate { get; set; }
+
+        public string Gender { get; set; }
+
+        public string EmailPrivate { get; set; }
+
+        public string EmailAssociaton { get; set; }
+
+        public string MobilePrivate { get; set; }
+
+        public string AdressStreet { get; set; }
+
+        public string AdressNr { get; set; }
+
+        public string AdressZIP { get; set; }
+
+        public string AdressCity { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+             
     }
 }
