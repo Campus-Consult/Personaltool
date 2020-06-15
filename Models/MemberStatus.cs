0c81ee91f51b208f5d7fd124abf4262748ac8e8d
Ediz Güney <edizapple@hotmail.com>
2020-06-15 19:51:21 +0200

    create model for member status

---
+++ Models/MemberStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Personaltool.Models
+{
+    public class MemberStatus
+    {
+        [Key]
+        public int MemberStatusID { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        public ICollection PersonsMemberStatus { get; set; }
+
+
+    }
+}
