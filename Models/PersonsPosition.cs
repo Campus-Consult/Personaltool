6fe8562da035bbaaba3a8dd38ab3cfda29d708fe
Andreas Pingel <a_pingel@mail.uni-paderborn.de>
2020-06-20 14:25:25 +0200

    Added validation for attributes in PersonsPosition.cs and Positions.cs

--- Models/PersonsPosition.cs
+++ Models/PersonsPosition.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,18 +8,30 @@ namespace Personaltool.Models
 {
     public class PersonsPosition
     {
+        [Key]
         public int PersonPositionID { get; set; }
 
+        [Required]
         public int PersonID { get; set; }
 
+        [Required]
         public int PositionID { get; set; }
 
+        [Required]
+        [Display(Name = "Startdatum")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Begin { get; set; }
 
+        [Display(Name = "Enddatum")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime End { get; set; }
 
+        [Required]
         public Person Person { get; set; }
 
+        [Required]
         public Position Position { get; set; }
     }
 }
--- Models/Position.cs
+++ Models/Position.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +9,20 @@ namespace Personaltool.Models
 {
     public class Position
     {
+        [Key]
         public int PositionID { get; set; }
 
+        [Required]
+        [Display(Name = "Bezeichnung")]
+        [DataType(DataType.Text)]
         public string Name { get; set; }
 
+        [Required]
+        [Display(Name = "Kurzbezeichnung")]
         public string ShortName { get; set; }
 
+        [Required]
+        [Display(Name = "Aktiv")]
         public Boolean IsActive { get; set; }
 
         public ICollection<Position> PersonsPositions { get; set; }
