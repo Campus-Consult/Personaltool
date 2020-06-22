c8035ceb193363baaa783b21ebc290d152a2b4f3
Yorreck Wagner <ywagner@campus-consult.org>
2020-06-22 14:46:27 +0200

    Create PersonIndexViewModel

---
+++ ViewModels/Person/PersonIndexViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Personaltool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Personaltool.ViewModels.Person
+{
+    public class PersonIndexViewModel
+    {
+        [Required]
+        [HiddenInput]
+        public int PersonID { get; set; }
+
+        [Required]
+        [Display(Name = "Vorname")]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Nachname")]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+
+        public PersonIndexViewModel(Models.Person person)
+        {
+            this.PersonID = person.PersonID;
+            this.FirstName = person.FirstName;
+            this.LastName = person.LastName;
+        }
+    }
+}
