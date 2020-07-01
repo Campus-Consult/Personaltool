76200f16afbc1b0868367c5a104337a23242d6dc
olohr <olohr@campus-consult.org>
2020-07-01 18:12:47 +0200

    Added defaut database seed for CC data

--- Program.cs
+++ Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Personaltool.Data;
 
 namespace Personaltool
 {
@@ -13,7 +15,17 @@ namespace Personaltool
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var webHost = CreateHostBuilder(args).Build();
+            using (var scope = webHost.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                // auto migration only on release
+                // context.Database.Migrate();
+
+                CCDefaultDataSeed.DoSeed(context).Wait();
+            }
+            webHost.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
