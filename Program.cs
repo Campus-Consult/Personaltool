dbacb92fd5b0c9f2b6dde83c206f20d5d7d9c783
olohr <olohr@campus-consult.org>
2020-07-03 17:27:34 +0200

    fix build failing in Release mode

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
