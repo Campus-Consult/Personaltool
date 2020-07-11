431bcc7ef799b63847299b17f108d90700d19510
olohr <olohr@campus-consult.org>
2020-07-11 23:56:57 +0200

    set correct status code in response

--- Helpers/ErrorExtension.cs
+++ Helpers/ErrorExtension.cs
@@ -14,6 +14,7 @@ namespace Personaltool.Helpers
     public static class ErrorExtension {
         public static IActionResult StatusCodeMessage(this Controller controller, int statusCode, String message = null) {
             message ??= ((HttpStatusCode)statusCode).ToString();
+            controller.HttpContext.Response.StatusCode = statusCode;
             return controller.View("Error", new ErrorViewModel { StatusCode = statusCode, ErrorMsg = message });
         }
     }
