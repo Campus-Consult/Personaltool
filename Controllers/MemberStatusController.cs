e8b1e6023e8bf9f2031cbd212f33b734d73c925d
Andreas Pingel <a_pingel@mail.uni-paderborn.de>
2020-07-29 18:10:26 +0200

    Added first list view. Buttons without functions.

--- Controllers/MemberStatusController.cs
+++ Controllers/MemberStatusController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Personaltool.Data;
+using Personaltool.Models;
 
 namespace Personaltool.Controllers
 {
     public class MemberStatusController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public MemberStatusController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Person/Index/
         public IActionResult Index()
         {
-            return View();
+            var memberStatus = _context.MemberStatus.
+                Include(x => x.PersonsMemberStatus);
+            return View(memberStatus);
         }
     }
 }
