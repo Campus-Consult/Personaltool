c09e0774556c70b5b514ddc02b98ca8c02bd99b8
Yorreck Wagner <ywagner@campus-consult.org>
2020-07-29 16:59:53 +0200

    Add simple Person partial views

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -11,9 +11,6 @@ using Personaltool.ViewModels.Person;
 
 namespace Personaltool.Controllers
 {
-    /// <summary>
-    /// PersonController, controlls person managing actions 
-    /// </summary>
     [Authorize]
     public class PersonController : Controller
     {
@@ -24,24 +21,135 @@ namespace Personaltool.Controllers
             _context = context;
         }
 
-        // GET: /Person/Index/
-        public async Task<IActionResult> IndexAsync()
-        {
-            List<Person> persons = await _context.Persons.ToListAsync<Person>();
-            List<PersonIndexViewModel> viewModels = persons.ConvertAll<PersonIndexViewModel>(person => new PersonIndexViewModel(person));
-            viewModels.Sort((x, y) => x.FirstName.CompareTo(y.FirstName));
-            return View(viewModels);
-        }
-
-        // Partial view
-        // GET: /Person/_PersonDetailsPartial/
-        public async Task<IActionResult> PersonDetailsPartial(int id)
+        // GET: Person
+        public IActionResult Index()
         {
             var persons = _context.Persons.Include(x => x.PersonsMemberStatus).ThenInclude(x => x.MemberStatus).Include(x => x.PersonsCareerLevels).ThenInclude(x => x.CareerLevel).Include(x => x.PersonsPositions).ThenInclude(x => x.Position);
-            Person person = await persons.FirstOrDefaultAsync(x => x.PersonID == id);
-            PersonDetailsViewModel viewModel = new PersonDetailsViewModel(person);
 
-            return PartialView("_PersonDetailsPartial", viewModel);
+            return View(persons);
+        }
+
+        // GET: Person/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var persons = _context.Persons.Include(x => x.PersonsMemberStatus).ThenInclude(x => x.MemberStatus).Include(x => x.PersonsCareerLevels).ThenInclude(x => x.CareerLevel).Include(x => x.PersonsPositions).ThenInclude(x => x.Position);
+            var person = await persons.FirstOrDefaultAsync(m => m.PersonID == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(new PersonDetailsViewModel(person));
+        }
+
+        // GET: Person/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PersonID,FirstName,LastName,Birthdate,Gender,EmailPrivate,EmailAssociaton,MobilePrivate,AdressStreet,AdressNr,AdressZIP,AdressCity")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(person);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: Person/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        // POST: Person/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PersonID,FirstName,LastName,Birthdate,Gender,EmailPrivate,EmailAssociaton,MobilePrivate,AdressStreet,AdressNr,AdressZIP,AdressCity")] Person person)
+        {
+            if (id != person.PersonID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(person);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PersonExists(person.PersonID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        // GET: Person/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _context.Persons
+                .FirstOrDefaultAsync(m => m.PersonID == id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var person = await _context.Persons.FindAsync(id);
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.Persons.Any(e => e.PersonID == id);
         }
     }
 }
