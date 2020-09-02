9a73a1b5b1017f30ae4f283eb2809da2d8255930
Alfer-Huber [Adrian] <53966598+Alfer-Huber@users.noreply.github.com>
2020-09-02 15:53:38 +0200

    deleted second File

--- ClientApp/src/app/models/person.class.ts
+++ ClientApp/src/app/models/person.class.ts
@@ -1,50 +1,46 @@
-interface PersonBase {
-  getDisplayName(property: string): string;
-}
-
-export interface Person extends PersonBase {
+export interface Person  {
   PersonID: number;
 
-  /** Display Name */
+  /**  */
   FirstName: string;
 
-  /** Display Name */
+  /**  */
   LastName: string;
 
-  /** Display Name */
+  /**  */
   Birthdate?: Date;
 
-  /** Display Name */
+  /**  */
   Gender?: Gender;
 
-  /** Display Name */
+  /**  */
   EmailPrivate?: string;
 
-  /** Display Name */
+  /**  */
   EmailAssociaton?: string;
 
-  /** Display Name */
+  /**  */
   MobilePrivate?: string;
 
-  /** Display Name */
+  /**  */
   AdressStreet?: string;
 
-  /** Display Name */
+  /**  */
   AdressNr?: string;
 
-  /** Display Name */
+  /**  */
   AdressZIP?: string;
 
-  /** Display Name */
+  /**  */
   AdressCity?: string;
 
-  /** Display Name */
+  /**  */
   PersonsMemberStatus: PersonsMemberStatus[];
 
-  /** Display Name */
+  /**  */
   PersonsCareerLevels: PersonsCareerLevel[];
 
-  /** Display Name */
+  /**  */
   PersonsPositions: PersonsPosition[];
 }
 
@@ -56,17 +52,17 @@ export enum Gender {
   DIVERSE = 'divers',
 }
 
-export interface PersonsMemberStatus extends PersonBase {
+export interface PersonsMemberStatus  {
   PersonsMemberStatusID: number;
 
   PersonID: number;
 
   MemberStatusID: number;
 
-  /** Display Name */
+  /**  */
   Begin: Date;
 
-  /** Display Name */
+  /**  */
   End?: Date;
 
   Person?: Person;
@@ -77,60 +73,62 @@ export interface PersonsMemberStatus extends PersonBase {
 export interface MemberStatus {
   MemberStatusID: number;
 
-  /** Display Name */
+  /**  */
   Name: string;
 
   PersonsMemberStatus?: PersonsMemberStatus[];
 }
 
-interface PersonsCareerLevel extends PersonBase {
+interface PersonsCareerLevel  {
   PersonsCareerLevelID: number;
 
   PersonID: number;
 
   CareerLevelID: number;
 
-  /** Display Name */
+  /**  */
   Begin: Date;
 
-  /** Display Name; optional*/
+  /**  optional*/
   End?: Date;
 
-  Person: Person;
+  /**  optional*/
+  Person?: Person;
 
-  /** Display Name */
+  /**  */
   CareerLevel: CareerLevel;
 }
 
-export interface CareerLevel extends PersonBase {
+export interface CareerLevel  {
   CareerLevelID: number;
 
-  /** Display Name */
+  /**  */
   Name: string;
 
-  /** Display Name */
+  /**  */
   ShortName: string;
 
-  /** Display Name */
+  /**  */
   IsActive: boolean;
 
   PersonsCareerLevels: PersonsCareerLevel[];
 }
 
-export interface PersonsPosition extends PersonBase {
+export interface PersonsPosition  {
   PersonPositionID: number;
 
   PersonID: number;
 
   PositionID: number;
 
-  /** Display Name */
+  /**  */
   Begin: Date;
 
-  /** Display Name; optional */
-  End: Date;
+  /**  optional */
+  End?: Date;
 
-  Person: Person;
+  /**  optional */
+  Person?: Person;
 
   Position: Position;
 }
