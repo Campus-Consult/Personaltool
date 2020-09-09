2d15d1b34e7d4af9c1b3d46a88bad8f7710b8da2
Alfer-Huber [Adrian] <53966598+Alfer-Huber@users.noreply.github.com>
2020-09-09 12:06:42 +0200

    issue55: simple person edit scaffold

--- ClientApp/src/app/personal/person-edit/person-edit.component.ts
+++ ClientApp/src/app/personal/person-edit/person-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Person } from 'src/app/models/person.class';
 
 @Component({
   selector: 'app-person-edit',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonEditComponent implements OnInit {
 
+  @Input()
+  personDetails: Person;
+
+  public displayedSubtitle: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.displayedSubtitle = this.personDetails.firstName + ' ' + this.personDetails.lastName;
   }
 
 }
