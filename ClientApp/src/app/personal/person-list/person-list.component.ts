1538d38129f6d1ec0f5f772c09f59d68233956ee
Alfer-Huber [Adrian] <53966598+Alfer-Huber@users.noreply.github.com>
2020-09-09 12:02:44 +0200

    issue55: Simple Person List

--- ClientApp/src/app/personal/person-list/person-list.component.ts
+++ ClientApp/src/app/personal/person-list/person-list.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
+import { PersonTableData } from '../personal.component';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-person-list',
   templateUrl: './person-list.component.html',
-  styleUrls: ['./person-list.component.scss']
+  styleUrls: ['./person-list.component.scss'],
 })
 export class PersonListComponent implements OnInit {
+  @Input()
+  personalData: PersonTableData[];
 
-  constructor() { }
+  @Input()
+  displayedColumns: string[];
+
+  @Output()
+  itemSelectedEvent = new EventEmitter<number>();
+
+  //dataSource = new MatTableDataSource<PersonTableData>(this.personalData);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor() {}
 
   ngOnInit(): void {
+    if (!this.displayedColumns) {
+      this.displayedColumns = [
+        'firstName',
+        'lastName',
+        'personsMemberStatus',
+        'personsCareerLevel',
+        'personsPosition',
+        'buttons',
+      ];
+    }
   }
 
+
+  ngAfterViewInit() {
+    // this.dataSource.paginator = this.paginator;
+  }
+
+  /** =============Person Action Methods ============== */
+
+  edit(persID: number) {}
+
+  details(persID: number) {}
+
+  delete(persID: number) {}
 }
