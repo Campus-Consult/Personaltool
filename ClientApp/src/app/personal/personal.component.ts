1ea8c54b23940ffc0eb62f57fba28a98751fd5f3
Alfer-Huber [Adrian] <53966598+Alfer-Huber@users.noreply.github.com>
2020-09-09 12:07:03 +0200

    issue55: Personal list and person details next to each other;

--- ClientApp/src/app/personal/personal.component.ts
+++ ClientApp/src/app/personal/personal.component.ts
@@ -1,15 +1,107 @@
 import { Component, OnInit } from '@angular/core';
+import { Person, Gender } from '../models/person.class';
 
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.component.html',
-  styleUrls: ['./personal.component.scss']
+  styleUrls: ['./personal.component.scss'],
 })
 export class PersonalComponent implements OnInit {
+  public personalTableData: PersonTableData[];
 
-  constructor() { }
+  public searchValue= '';
+
+  public selectedPerson: Person;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.personalTableData = new Array<PersonTableData>();
+    for (let index = 0; index < 10; index++) {
+      this.personalTableData.push({
+        personID: index,
+        firstName: 'Test' + index,
+        lastName: 'Subject' + index,
+        personsMemberStatus: 'MembverStautus',
+        personsCareerLevel: 'Careerlevel',
+        personsPosition: 'personsPositions'
+
+      });      
+    }
+    setTimeout(() => {
+      console.log(this.searchValue);
+    }, 20000);
+
   }
 
+  createPerson() {}
+
+  getPersonDetails(personID: number): Person{
+    const personList = new Array<Person>();
+    for (let index = 0; index < 10; index++) {
+      
+      personList.push({
+        personID: index,
+        firstName: 'Test' + index,
+        lastName: 'Subject' + index,
+
+        birthdate: new Date(index),
+
+        gender: index%2===0? Gender.MALE: Gender.FEMALE,
+      
+        emailPrivate: 'testMail@webkitCancelAnimationFrame.de',
+    
+        emailAssociaton: 'testMail@webkitCancelAnimationFrame.de',
+      
+        mobilePrivate: "+49123456789",
+      
+        /**  */
+        adressStreet: 'Stra0enweg',
+      
+        /**  */
+        adressNr: (index + 56).toString(),
+      
+        /**  */
+        adressCity: 'paderborn',
+      
+        /**  */
+        personsMemberStatus: [{
+          personID: index,
+          personsMemberStatusID: index,
+          memberStatusID: index,
+          begin: new Date()
+        }],
+      
+        /**  */
+        personsCareerLevels: [{
+          personID: index,
+          personsCareerLevelID: index,
+          careerLevelID: index,
+          begin: new Date()
+        }],
+      
+        /**  */
+        personsPositions: [{
+          personID: index,
+          personPositionID: index,
+          positionID: index,
+          begin: new Date()
+        }]
+      })
+    }
+
+    return personList.find((val)=>val.personID === personID);
+  }
 }
+
+export interface PersonTableData{
+  personID: number
+  firstName: string;
+  lastName: string;
+  personsMemberStatus: string;
+  personsCareerLevel: string;
+  personsPosition: string;
+}
+
+
+
--- ClientApp/src/app/personal/personal.module.ts
+++ ClientApp/src/app/personal/personal.module.ts
@@ -4,14 +4,37 @@ import { PersonalComponent } from './personal.component';
 import { PersonEditComponent } from './person-edit/person-edit.component';
 import { PersonDetailsComponent } from './person-details/person-details.component';
 import { PersonListComponent } from './person-list/person-list.component';
-
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { FormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
-  declarations: [PersonalComponent, PersonEditComponent, PersonDetailsComponent, PersonListComponent],
+  declarations: [
+    PersonalComponent,
+    PersonEditComponent,
+    PersonDetailsComponent,
+    PersonListComponent,
+  ],
   exports: [PersonalComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    MatCardModule,
+    MatExpansionModule
+  ],
 })
-export class PersonalModule { }
+export class PersonalModule {}
