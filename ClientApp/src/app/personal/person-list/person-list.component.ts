e1a54e4ff13db50e0472ca07f082fa2dee22a516
Alfer-Huber [Adrian] <53966598+Alfer-Huber@users.noreply.github.com>
2020-09-30 12:53:09 +0200

    Issue 55: Added Search, List and  Filter, deutsch

--- ClientApp/src/app/personal/person-list/person-list.component.ts
+++ ClientApp/src/app/personal/person-list/person-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angu
 import { PersonTableData } from '../personal.component';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-person-list',
@@ -12,6 +13,8 @@ export class PersonListComponent implements OnInit {
   @Input()
   personalData: PersonTableData[];
 
+  dataSource: MatTableDataSource<PersonTableData>;
+
   @Input()
   displayedColumns?: string[];
 
@@ -21,6 +24,14 @@ export class PersonListComponent implements OnInit {
   //dataSource = new MatTableDataSource<PersonTableData>(this.personalData);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  
+  public selectedPerson: PersonTableData;
+
+  
+  public searchValue = '';
+
 
   constructor() {}
 
@@ -32,14 +43,22 @@ export class PersonListComponent implements OnInit {
         'personsMemberStatus',
         'personsCareerLevel',
         'personsPosition',
-        'buttons',
+        'buttons'
       ];
     }
+
+    this.dataSource = new MatTableDataSource(this.personalData);
   }
 
 
   ngAfterViewInit() {
-    // this.dataSource.paginator = this.paginator;
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   /** =============Person Action Methods ============== */
